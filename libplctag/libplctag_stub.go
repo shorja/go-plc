21c0df7433e536a027a0caab1c5bce7cb456658b
James <james.m.short@gmail.com>
2022-11-01 15:07:34 -0600

    added print to test if this is being included properly

--- libplctag/libplctag_stub.go
+++ libplctag/libplctag_stub.go
@@ -1,11 +1,20 @@
+//go:build stub
 // +build stub
 
 package libplctag
+
+import "fmt"
 
 /*
 #cgo LDFLAGS: -lplctagstub
 #include <libplctag.h>
 */
 import "C"
+
+func init() {
+
+	fmt.Println("Hello from go-plc stub!")
+
+}
 
 const StubActive = true
